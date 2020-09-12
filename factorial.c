846dd87c9fa97fdb0769ea52cce309c4afd3038c
Vansh <vansharora2929@gmail.com>
2020-09-12 21:38:43 +0530

    main to test

--- factorial.c
+++ factorial.c
@@ -4,5 +4,13 @@ int factorial(int num)
     if(num <= 1)
         return 1;
     
-    return num * factorial(num - 1)
+    return num * factorial(num - 1);
+}
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d",factorial(a));
+    return 0;
 }
